3972afe945987350dad56177b055a8d68bc534c8
Mayank Jaiswal <jaiswalmayank450@gmail.com>
2024-12-09 23:49:09 +0530

    step7

--- demo/server.js
+++ demo/server.js
@@ -42,19 +42,9 @@ io.on('connection', (socket) => {
         content: content,
       });
             console.log(`Private message from ${socket.id} to ${socketMap[to]}: ${content}`);
-
     }
   });
 
-  //   socket.on('private message', ({ to, message }) => {
-  //   const fromUser = socketMap[socket.id];
-  //   if (socketMap[to]) {
-  //     io.to(to).emit('private message', { from: fromUser, message });
-  //     console.log(`Private message from ${fromUser.username} to ${socketMap[to].username}: ${message}`);
-  //   } else {
-  //     socket.emit('error', { message: 'User not found or disconnected' });
-  //   }
-  // });
 
   io.emit('newConnection',socketMap); // Broadcast message
 
@@ -81,30 +71,6 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
-
-// function haiku(){
-//   var adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry",
-//   "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring",
-//   "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered",
-//   "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
-//   "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
-//   "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
-//   "wandering", "withered", "wild", "black", "young", "holy", "solitary",
-//   "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
-//   "polished", "ancient", "purple", "lively", "nameless"]
-
-//   , nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea",
-//   "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn",
-//   "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird",
-//   "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower",
-//   "firefly", "feather", "grass", "haze", "mountain", "night", "pond",
-//   "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf",
-//   "thunder", "violet", "water", "wildflower", "wave", "water", "resonance",
-//   "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
-//   "frog", "smoke", "star"];
-
-//   return adjs[Math.floor(Math.random()*(adjs.length-1))]+"_"+nouns[Math.floor(Math.random()*(nouns.length-1))].toUpperCase();
-// }
 
 function animeUsername() {
   var title = [
